231ffbf382cadea8e786decfd32d6740b266d6c0
unknown <msuch92@gmail.com>
2022-05-31 09:33:54 +0200

    quit shortcut added

--- mqb_script.py
+++ mqb_script.py
@@ -7,22 +7,6 @@ import time
 
 import os
 
-# get parametrization string
-param_string = input('Paste parametrization string\n')
-
-# read or write
-action = input('pres S to set or R - to read and confirm with enter\n')
-
-if action.lower() == 's':
-    print('save frame')
-    # set parameters
-    frame_to_send = SetFrame(param_string)
-else:
-    print('read frame')
-    # read parameters
-    frame_to_send = ReadFrame(param_string)
-
-print('LEN:{}, SID: {}, DATA: {}'.format(frame_to_send.payload_len, frame_to_send.data[0], frame_to_send.data[1:]))
 
 # initialize BabyLin
 BabyLin = BL()
@@ -31,44 +15,69 @@ BabyLin.loadSdf(lin.sdf)
 BabyLin.startSchedule(lin.schedule_num)  # diagnostic session
 BabyLin.macro_exec(lin.extended_session_macro)  # exetended session frame - periodic send
 
-#  signals
-signals_handler = signalsHandler(BabyLin, frame_to_send)
-signals_handler.assign_signals()
-
-# setup response to 0 - to be able to spot the difference if already correct response
-for signal in frame_config.read_range:
-    BabyLin.setsig(signal, 0)
-    time.sleep(0.01)
 
 # dissignal read signals
 for signal in frame_config.read_signals:
     BabyLin.dissignal(signal, 1)
     time.sleep(0.0)
 
-# send frame
-BabyLin.macro_exec(lin.parameter_macro)
-time.sleep(0.5)
 
-response = []
-for signal in frame_config.read_signals:
-    response.append(BabyLin.readSignal(signal))
-    time.sleep(0.01)
+while True:
 
-# if read skip first 3 bytes
-if not action.lower() == 's':
-    response = response[3:]
+    # read or write
+    action = input('press:\n - S to set\n - R - to read \n - Q - to quit \nThan confirm with enter\n')
+    if action.lower() == 'q':
+        break
 
-hex_response = ''.join([hex(x)[2:].zfill(2) for x in response])
-print(hex_response)  # dec to hex
+    # get parametrization string
+    param_string = input('Paste parametrization string\n')
+    
+    
+    if action.lower() == 's':
+        print('save frame')
+        # set parameters
+        frame_to_send = SetFrame(param_string)
+    else:
+        print('read frame')
+        # read parameters
+        frame_to_send = ReadFrame(param_string)
+    
+    print('LEN:{}, SID: {}, DATA: {}'.format(frame_to_send.payload_len, frame_to_send.data[0], frame_to_send.data[1:]))
+    
+    
+    #  signals
+    signals_handler = signalsHandler(BabyLin, frame_to_send)
+    signals_handler.assign_signals()
+    
+    # setup response to 0 - to be able to spot the difference if already correct response
+    for signal in frame_config.read_range:
+        BabyLin.setsig(signal, 0)
+        time.sleep(0.01)
+    
+    # send frame
+    BabyLin.macro_exec(lin.parameter_macro)
+    time.sleep(0.5)
+    
+    response = []
+    for signal in frame_config.read_signals:
+        response.append(BabyLin.readSignal(signal))
+        time.sleep(0.01)
+    
+    # if read skip first 3 bytes
+    if not action.lower() == 's':
+        response = response[3:]
+    
+    hex_response = ''.join([hex(x)[2:].zfill(2) for x in response])
+    print(hex_response)  # dec to hex
+    print('\nDone\n')
+    
+    with open("last_resp.txt", 'w', encoding='utf-8') as f:
+        f.write('\n'+str(hex_response))
+    
+    # if read copy answer to clipboard
+    if not action.lower() == 's':
+        pyperclip.copy(hex_response)
 
 
-with open("last_resp.txt", 'w', encoding='utf-8') as f:
-    f.write('\n'+str(hex_response))
-
-# if read copy answer to clipboard
-if not action.lower() == 's':
-    pyperclip.copy(hex_response)
 # close BabyLin
 BabyLin.closeAll()
-
-input('Press Enter to close')
